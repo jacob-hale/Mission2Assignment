e6c53a613ba52e9673d1b14467d80953315196c6
Jacob Hale <Jchale23@byui.edu>
2026-01-14 17:19:54 -0700

    Add project files.

---
+++ DiceRoll.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mission2Assignment
+{
+    internal class DiceRoll
+    {
+        private int NumRolls;
+        private Random rng = new Random();
+        private int[] Combos = new int[11];
+        
+        // Constructor
+        public DiceRoll(int NumRolls)
+        {
+            this.NumRolls = NumRolls;
+        }
+
+        // Roll method returns an array with the counnts of each number combination
+        public int[] Roll()
+        {
+            for(int i = 0; i < NumRolls; i++)
+            {
+                // Randomly generates the two values, adds them, and puts them into the right index in the array
+                int roll1 = rng.Next(1, 7);
+                int roll2 = rng.Next(1, 7);
+                int total = roll1 + roll2;
+                Combos[total - 2]++;
+            }
+            // Returns completed array
+            return Combos;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Mission2Assignment;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int NumRolls;
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        Console.WriteLine("How many dice rolls would you like to simulate? ");
+        NumRolls = int.Parse(Console.ReadLine());
+        Console.WriteLine("===========================================");
+        Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each '*' represents 1% of the total number of rolls.");
+        Console.WriteLine("Total number of rolls = " + NumRolls);
+        Console.WriteLine("===========================================");
+
+        // run the DiceRoll constructor and save to value
+        DiceRoll diceSimulator = new DiceRoll(NumRolls);
+
+        // run the roll method from that instance with the NumRolls
+        int[] results = diceSimulator.Roll();
+
+        // iterate through the array it returns and output the count of combinations
+        for (int i = 0; i < results.Length; i++)
+        {
+            // floor division to find i's count's percentage of total
+            int starCount = (int)Math.Round(((double)results[i] / NumRolls) * 100, MidpointRounding.AwayFromZero);
+            string stars = new string('*', starCount);
+            Console.WriteLine(i + 2 + ": " + stars);
+        }
+
+        Console.WriteLine("===========================================");
+        Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+    }
+}
